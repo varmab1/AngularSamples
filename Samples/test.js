9d52d00bfd189d5a89c188ecfbc63f3116f3ada2
Pavan Varma Bhupatiraju <bpavanvarma17@gmail.com>
2023-06-05 11:21:12 -0500

    Samples

---
+++ Samples/test.js
@@ -0,0 +1,12 @@
+//Arrow Functions
+
+const materials = [
+    'Hydrogen',
+    'Helium',
+    'Lithium',
+    'Beryllium'
+  ];
+  
+  console.log(materials.map(material => material.length));
+  var a=1
+  console.log((a => materials[a])());
