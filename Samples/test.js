00110b6fe57e677519d48f7bad4ce6b3f2713634
varma <varma.vb76@gmail.com>
2023-06-15 08:23:26 -0500

    all changes

--- Samples/test.js
+++ Samples/test.js
@@ -9,4 +9,20 @@ const materials = [
   
   console.log(materials.map(material => material.length));
   var a=1
-  console.log((a => materials[a])());
+  console.log((a => materials[a])(1));
+
+  var x={lis:[],lis1:[]}
+  x.__proto__.boo= function (ele){
+    this.lis1.push(ele)
+  }
+
+
+  x.far= function(ele){
+    this.lis.push(ele);
+  }
+
+  x.far(10)
+  x.boo(11)
+  console.log(x.lis[0])
+  
+
